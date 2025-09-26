2accbb779759297be983ba40eceedd1657f56d69
Patryk Karaszewski <karaszewski21@gmail.com>
2025-09-26 12:18:47 +0200

    added base implements page post-list, post details, implements routing, added  list post in order to test routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/posts/pages/post-list/post-list.component').then(m => m.PostListComponent)
+    },
+    {
+        path: 'post-details/:id',
+        loadComponent: () => import('./features/posts/pages/post-details/post-details.component').then(m => m.PostDetailsComponent)
+    },
+];
---
+++ src/app/features/posts/pages/post-details/post-details.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'app-post-details',
+    templateUrl: './post-details.component.html',
+})
+export class PostDetailsComponent {
+    constructor() { }
+}
